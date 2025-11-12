41cbc61e2184b479c9f54904e2d66d21fb961779
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-11-12 15:38:48 +0900

    3-4층 플레이어 및 AI
    플레이어 구르기, 대시, 슬라이드, 웅크리기, 카메라 전환, 무기 변경, 무기 해제, 상호작용
    AI Combat, Patrol
    Damage System + Ragdoll, Die
    4층 폭발물 스위치 상호작용 + 5층 진입문 확보

--- KillerRestaurantV2/KillerRestaurantV2/Source/KillerRestaurantV2/Public/MyAIController.h
+++ KillerRestaurantV2/KillerRestaurantV2/Source/KillerRestaurantV2/Public/MyAIController.h
@@ -17,22 +17,5 @@ class KILLERRESTAURANTV2_API AMyAIController : public AAIController
 public:
     AMyAIController();
 
-protected:
-    virtual void BeginPlay() override;
-    virtual void OnPossess(APawn* InPawn) override;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MySettings")
-    class UAIPerceptionComponent* PerceptionComp;
-
-    UPROPERTY()
-    class UAISenseConfig_Sight* SightConfig;
-
-    UPROPERTY()
-    class UAISenseConfig_Hearing* HearingConfig;
-
-    UFUNCTION()
-    void OnTargetPerceived(AActor* Actor, FAIStimulus Stimulus);
-
-    UPROPERTY(EditDefaultsOnly, Category = "MySettings")
-    class UBehaviorTree* BehaviorTreeAsset;
 };
